83a713321af58e64728ee9dc4cf9b6b3c6605159
Tom Kerr <tomkerrdev@gmail.com>
2022-10-10 20:15:13 +0100

    Initial bot setup

---
+++ bot/main.py
@@ -0,0 +1,6 @@
+from sc2.bot_ai import BotAI
+
+
+class Kitten(BotAI):
+    async def on_step(self, iteration: int) -> None:
+        pass
---
+++ run.py
@@ -0,0 +1,63 @@
+import random
+import sys
+
+sys.path.insert(1, "python-sc2")
+
+from sc2 import maps
+from sc2.data import Race, Difficulty
+from sc2.main import run_game
+from sc2.player import Bot, Computer, AIBuild
+from ladder import run_ladder_game
+
+from bot.main import Kitten
+
+bot1 = Bot(Race.Terran, Kitten(), "kitten")
+
+
+def main():
+    # Ladder game started by LadderManager
+    print("Starting ladder game...")
+    result, opponentid = run_ladder_game(bot1)
+    print(result, " against opponent ", opponentid)
+
+
+# Start game
+if __name__ == "__main__":
+    if "--LadderServer" in sys.argv:
+        # Ladder game started by LadderManager
+        print("Starting ladder game...")
+        result, opponentid = run_ladder_game(bot1)
+        print(result, " against opponent ", opponentid)
+    else:
+        # Local game
+        random_map = random.choice(
+            [
+                # "BerlingradAIE",
+                # "InsideAndOutAIE",
+                # "MoondanceAIE",
+                # "StargazersAIE",
+                "WaterfallAIE",
+                # "HardwireAIE",
+            ]
+        )
+        random_race = random.choice(
+            [
+                Race.Zerg,
+                # Race.Terran,
+                # Race.Protoss
+            ]
+        )
+        print("Starting local game...")
+        run_game(
+            maps.get(random_map),
+            [
+                # bot2,
+                bot1,
+                Computer(random_race, Difficulty.CheatVision, ai_build=AIBuild.Macro),
+                # bot2,
+            ],
+            realtime=False,
+            # save_replay_as="ZvTMuta.SC2Replay",
+            # 2 lower spawn / 2564 upper spawn
+            # random_seed=2,
+        )
